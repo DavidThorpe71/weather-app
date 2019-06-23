2a9dfdf2d430c3b9d2be59c3048e15493aafdd35
David Thorpe <davidthorpe878@gmail.com>
2019-06-23 14:50:58 +0100

    Added dotenv support to safely store api keys

--- weather-app-server/index.js
+++ weather-app-server/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const port = 4000;
 const app = express();
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
 app.use(bodyParser.json());
+
+const { DARK_SKY_KEY } = process.env;
 
 app.get("/", (req, res) => {
   res.send("Hello");
