2603265726627bd6acc62251c9656928b5215dea
David Thorpe <davidthorpe878@gmail.com>
2019-06-23 15:05:18 +0100

    Add proxy address and getWeather function

--- weather-app-client/src/App.js
+++ weather-app-client/src/App.js
@@ -2,11 +2,24 @@ import React, { PureComponent } from "react";
 import "./App.css";
 
 class App extends PureComponent {
+  getWeather = async () => {
+    const data = await fetch("/weather", {
+      method: "Post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ requestedLocation: "Singapore" })
+    }).then(res => res.json());
+    console.log({ data });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>Weather app</p>
+          <button onClick={this.getWeather}>Get weather test</button>
         </header>
       </div>
     );
